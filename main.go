11c104b92c7afe2bf269c49b3af57672dbf50257
Aaronstotle <aaron.wasserman@berkeley.edu>
2023-07-17 12:57:09 -0700

    setting up scafflolding for program/logic flow

--- main.go
+++ main.go
@@ -26,26 +26,25 @@ func main() {
 	}
 	defer db.Close()
 
-	stmt, err := db.Prepare("INSERT INTO users (username, password_hash, created_at) VALUES ($1, $2, $3)")
-	if err != nil {
-		log.Fatal(err)
+	// stmt, err := db.Prepare("INSERT INTO users (username, password_hash, created_at) VALUES ($1, $2, $3)")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer stmt.Close()
+
+	userExists := false
+
+	username := greeting() // Assign variables to output of functions to use in DB insert later
+	fmt.Printf("Your username is %s\n", username)
+	if userExists {
+		//TODO: Add DB Query to search for user and  passwords
+		fmt.Printf("User exists already, lets get your passwords\n")
+	} else {
+		fmt.Printf("User does not existing, writing to database")
 	}
-	defer stmt.Close()
-
-	greeting()
-	hashPass()
-
+	passwordhash := hashPass()
+	fmt.Println(passwordhash)
 	exit()
-
-	// var userString string
-	// bytePass, _ := term.ReadPassword(int(syscall.Stdin))
-
-	// userString = string(bytePass)
-	// h := sha256.New()
-	// h.Write([]byte(userString)) // this line actually hashes the password
-	// passwordHash := hex.EncodeToString(h.Sum(nil))
-	// fmt.Println(passwordHash)
-	// createdAt := time.Now()
 
 	// _, err = stmt.Exec(username, passwordHash, createdAt)
 	// if err != nil {
@@ -56,23 +55,25 @@ func main() {
 
 }
 
-func greeting() {
+func greeting() string {
+
 	var username string
+
 	fmt.Println("Please enter your username")
 	fmt.Scanf("%s", &username)
 
-	fmt.Println("Please enter your password, it will not show up when typing")
-
+	return username
 }
 
 func hashPass() string {
+	fmt.Println("Please enter your password, it will not show up when typed")
 	bytePass, _ := term.ReadPassword(int(syscall.Stdin))
 	pass := string(bytePass)
 
 	h := sha256.New()
 	h.Write([]byte(pass))
 	passwordHash := hex.EncodeToString(h.Sum(nil))
-	fmt.Printf("Your hashed password is, %x", h.Sum(nil))
+	//fmt.Printf("Your hashed password is, %x\n", h.Sum(nil))
 	return passwordHash
 }
 
